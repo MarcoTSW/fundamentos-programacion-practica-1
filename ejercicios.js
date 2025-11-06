3e27e3c71a9d2a88c2799cbe559557addfc25a7a
MarcoTSW <marco.parera@tecdesoftware.edu.mx>
2025-11-05 20:33:01 -0600

    ejercicio 4.3 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -270,7 +270,7 @@ function promedioArray(numeros) {
 function encontrarMaximo(numeros) {
   // TODO: Encuentra y retorna el nÃºmero mÃ¡s grande
   // Pista: Puedes usar Math.max(...numeros) o hacerlo con un bucle
-  return 0;
+  return Math.max(...numeros);
 }
 
 /**
